3dfc50969f23d5748dc454b7783b4699a440de60
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-07-27 13:49:54 +0500

    применены модули к card button

--- film-app/src/components/Card/Card.jsx
+++ film-app/src/components/Card/Card.jsx
@@ -2,18 +2,18 @@ import styles from './Card.styles.css';
 
 function Card() {
 	return (
-		<div className='card-item'>
-			<div className='rating-button'>
-				<img src="star.svg" alt="Изображение звезды" className='star' />
-				<p className='rating-button__star'>324</p>
+		<div className={styles['card-item']}>
+			<div className={styles['rating-button']}>
+				<img src="star.svg" alt="Изображение звезды" className={styles['star']}/>
+				<p className= {styles['rating-button__star']}>324</p>
 			</div>
 			
-			<img src="blackwidow.svg" alt="Постер фильма" className='poster' />
-			<div className='card-item__block'>
-				<h2 className='card-item__header'>Black Widow</h2>
-				<div className='card-item__favorite'>
-					<img src="like.svg" alt="Знак лайка" className='like'/>
-					<p className='card-item__favorite_text'>В избранное</p>
+			<img src="blackwidow.svg" alt="Постер фильма" className={styles['poster']} />
+			<div className={styles['card-item__block']}>
+				<h2 className={styles['card-item__header']}>Black Widow</h2>
+				<div className={styles['card-item__favorite']}>
+					<img src="like.svg" alt="Знак лайка" className={styles['like']}/>
+					<p className={styles['card-item__favorite_text']}>В избранное</p>
 				</div>
 			</div>
 			
